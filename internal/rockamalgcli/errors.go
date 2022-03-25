e6d17d85f6f6a8d84f6d673bd7a02b3397a2beaa
Aleksey Bakin <alexey.bakin@gmail.com>
2022-03-25 15:11:14 +0300

    refactor: extrac amalg logic from cli

--- internal/rockamalgcli/errors.go
+++ internal/rockamalgcli/errors.go
@@ -2,9 +2,4 @@ package rockamalgcli
 
 import "errors"
 
-var (
-	errRockspecDepsSimultaneously = errors.New("rockspec and deps are not allowed simultaneously")
-	errFirmwareMissed             = errors.New("firmware is missed")
-	errRockspecIsNotRegularFile   = errors.New("rockspec is not a regular file")
-	errOutputIsAbsolutePath       = errors.New("output file name should not be absolute")
-)
+var errOutputIsAbsolutePath = errors.New("output file name should not be absolute")
